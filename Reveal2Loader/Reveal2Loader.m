b9a231fb65b8009aa54185c69144b1cf4cf8ce34
yuxilong <305758560@qq.com>
2025-09-22 13:47:36 +0800

    update to v55

--- Reveal2Loader/Reveal2Loader.m
+++ Reveal2Loader/Reveal2Loader.m
@@ -14,18 +14,30 @@ CHConstructor // code block that runs immediately upon load
 {
     @autoreleasepool
     {
-        NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:@"/var/mobile/Library/Preferences/com.rheard.RHRevealLoader.plist"];
-        NSString *libraryPath = @"/Library/Frameworks/RevealServer.framework/RevealServer";
-        if([[prefs objectForKey:[NSString stringWithFormat:@"RHRevealEnabled-%@", [[NSBundle mainBundle] bundleIdentifier]]] boolValue]) {
-            if ([[NSFileManager defaultManager] fileExistsAtPath:libraryPath]){
-                void *addr = dlopen([libraryPath UTF8String], RTLD_NOW);
-                if(addr){
-                    [[NSNotificationCenter defaultCenter] postNotificationName:@"IBARevealRequestStart" object:nil];
-                    NSLog(@"Reveal2Loader loaded %@ successed, address %p", libraryPath,addr);
-                }
-                else{
-                    NSLog(@"Reveal2Loader loaded %@ failed, address %p", libraryPath,addr);
-                }
+//        NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:@"/var/jb//var/mobile/Library/Preferences/com.rheard.RHRevealLoader.plist"];
+//        NSString *libraryPath = @"/var/jb/Library/Frameworks/RevealServer.framework/RevealServer";
+//        if([[prefs objectForKey:[NSString stringWithFormat:@"RHRevealEnabled-%@", [[NSBundle mainBundle] bundleIdentifier]]] boolValue]) {
+//            if ([[NSFileManager defaultManager] fileExistsAtPath:libraryPath]){
+//                void *addr = dlopen([libraryPath UTF8String], RTLD_NOW);
+//                if(addr){
+//                    [[NSNotificationCenter defaultCenter] postNotificationName:@"IBARevealRequestStart" object:nil];
+//                    NSLog(@"Reveal2Loader loaded %@ successed, address %p", libraryPath,addr);
+//                }
+//                else{
+//                    NSLog(@"Reveal2Loader loaded %@ failed, address %p", libraryPath,addr);
+//                }
+//            }
+//        }
+
+        NSString *libraryPath = @"/var/jb/Library/Frameworks/RevealServer.framework/RevealServer";
+        if ([[NSFileManager defaultManager] fileExistsAtPath:libraryPath]){
+            void *addr = dlopen([libraryPath UTF8String], RTLD_NOW);
+            if(addr){
+                [[NSNotificationCenter defaultCenter] postNotificationName:@"IBARevealRequestStart" object:nil];
+                NSLog(@"Reveal2Loader loaded %@ successed, address %p", libraryPath,addr);
+            }
+            else{
+                NSLog(@"Reveal2Loader loaded %@ failed, address %p", libraryPath,addr);
             }
         }
     }
